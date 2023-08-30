b651c3db5861716db4d6b99ac81443d2f67ac1c8
JOYCE <joychinwenduibeh93@gmail.com>
2023-08-30 12:27:21 +0100

    authentication form using redux

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,9 @@
+/* Write your code here */
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+function PrivateRoute({ Component, auth }) {
+  return auth ? <Component /> : <Navigate to="/login" />;
+}
+
+export default PrivateRoute;
